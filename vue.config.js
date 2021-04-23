8916c1b50cadac4dd06f7b42fec677020f1647c8
mike.zhu <512122313@qq.com>
2021-04-23 15:52:17 +0800

    🔧build: webpack配置增加alias

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,23 @@
+var path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 module.exports = {
   pages: {
     index: {
       entry: "examples/main.js",
       template: "public/index.html",
       filename: "index.html",
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        src: resolve("src"),
+        utils: resolve("src/utils"),
+        packages: resolve("packages"),
+      },
     },
   },
 };
